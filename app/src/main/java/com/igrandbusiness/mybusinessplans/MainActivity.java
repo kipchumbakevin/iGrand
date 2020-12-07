a3b0d62b7009097fe33b366845ff9c50aaaf4e39
kipchumbakevin <kipchumbakevin28@gmail.com>
2020-12-07 22:07:41 +0300

    commit

--- app/src/main/java/com/igrandbusiness/mybusinessplans/MainActivity.java
+++ app/src/main/java/com/igrandbusiness/mybusinessplans/MainActivity.java
@@ -1,30 +1,57 @@
 package com.igrandbusiness.mybusinessplans;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
+    TextView greeting;
+    String phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greeting = findViewById(R.id.greeting);
+        phone = "+254795801971";
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_call:
+                                Intent intent = new Intent(Intent.ACTION_DIAL);
+                                intent.setData(Uri.parse("tel:"+ phone));
+                                startActivity(intent);
+                                return true;
+                            case R.id.navigation_media:
+                                return true;
+                        }
+                        return false;
+                    }
+                };
         greetUser();
     }
-    
+
     private void greetUser() {
         Calendar calendar = Calendar.getInstance();
         int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
         if (timeOfDay >= 0 && timeOfDay<12){
-            Toast.makeText(this, "Good morning", Toast.LENGTH_SHORT).show();
+            greeting.setText("Good morning");
         }else if (timeOfDay >=12 && timeOfDay < 16){
-            Toast.makeText(this, "Good afternoon", Toast.LENGTH_SHORT).show();
+            greeting.setText("Good afternoon");
         }else if (timeOfDay >= 16 && timeOfDay < 24) {
-            Toast.makeText(this, "Good evening", Toast.LENGTH_SHORT).show();
+            greeting.setText("Good evening");
         }
     }
 }
